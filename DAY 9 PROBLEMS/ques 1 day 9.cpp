da2bd8527f7335aa9b1bea3a04ebd150a858d979
AlokMaurya234 <114644812+AlokMaurya234@users.noreply.github.com>
2022-12-26 10:53:21 +0530

    Add files via upload

---
+++ DAY 9 PROBLEMS/ques 1 day 9.cpp
@@ -0,0 +1,7 @@
+class Solution {
+  public:
+    long long int nthOfSeries(long long int n){
+        // code here
+        return ((8*n*n)+1);
+    }
+};
---
+++ DAY 9 PROBLEMS/ques 8 day 9.c
@@ -0,0 +1,11 @@
+int trailingZeroes(int n){
+    int count=0;
+    while(n>0)
+    {
+        n=n/5;
+        count=count+n;
+
+    }
+    return count;
+
+}
